fe7595710c05017eccf28bd4457df1f07892bf28
ronaldo jose da silva <ronaldorjsilva@hotmail.com>
2016-06-27 21:41:56 -0300

    Referenciando Objeto

---
+++ ReferenciandoObjeto/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenciandoObjeto
+{
+    public class Cliente
+    {
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nome:" + Nome);
+            sb.AppendLine("Idade: " + Idade);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ReferenciandoObjeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenciandoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Cliente();
+            c1.Nome = "Ronaldo José da Silva";
+            c1.Idade = 29;
+
+            var c2 = c1; //aponta para o mesmo endereço de memória
+            c2.Nome = "Marcão Salustiano de Pontes";
+
+            var c3 = new Cliente();
+            c3.Nome = "Dora";
+            c3.Idade = 40;
+            Console.WriteLine(c3);
+            Console.WriteLine(c2);
+            Console.WriteLine(c1);
+            Console.ReadLine();
+        }
+    }
+}
