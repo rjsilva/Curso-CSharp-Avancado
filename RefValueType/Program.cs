1cc9e1427273fcc3e85736cc739761d7f8d6f6e9
ronaldo jose da silva <ronaldorjsilva@hotmail.com>
2016-06-27 21:19:13 -0300

    Value e Referência Types

---
+++ RefValueType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valores vl = new Valores();
+            //vl.Exemplo();
+            Numeros num = new Numeros();
+            num.X = 10;
+            num.Y = 20;
+
+            //Passa um obejto para multiplicar os números
+            vl.Dobrar(num);
+            Console.WriteLine(num.X);
+            Console.WriteLine(num.Y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RefValueType/Valores.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefValueType
+{
+    public class Valores
+    {
+        public int X { get; set; }
+        public int y { get; set; }
+
+
+        public void Exemplo()
+        {
+            //passa o valor por referência para multiplicar
+            int x = 10;
+            Dobrar(ref x);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        public void Dobrar(Numeros num)
+        {
+            num.X *= 2;
+            num.Y *= 2;
+        }
+
+        //recebe o valor por referência para multiplicar
+        public void Dobrar(ref int valor)
+        {
+            valor *= 2;
+
+        }
+    }
+}
