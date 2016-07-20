84245776d41220f9b1db82590af9e93977db2a23
ronaldo jose da silva <ronaldorjsilva@hotmail.com>
2016-07-19 23:07:59 -0300

    metodo assesores

---
+++ RefValueType/MetodoAssesores/Conta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoAssesores
+{
+    public class Conta
+    {
+        public String Cliente { get; set; }
+        private Double saldo;
+
+        public void depositar(double valor)
+        {
+            this.saldo = valor;
+        }
+
+        public void sacar(double valor)
+        {
+            this.saldo = saldo - valor;
+        }
+
+        public double consultar()
+        {
+            return this.saldo;
+        }
+    }
+}
---
+++ RefValueType/MetodoAssesores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoAssesores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Conta();
+            c1.Cliente = "Ronaldo";
+            c1.depositar(1000);
+            var consulta = c1.consultar();
+            Console.WriteLine("saldo " + consulta);
+            c1.sacar(200);
+            var consulta1 = c1.consultar();
+            Console.WriteLine("saldo " + consulta1);
+            Console.ReadLine();
+        }
+    }
+}
